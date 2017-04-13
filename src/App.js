c829045d07651e6bc7898da9927e17992e04fbd2
frizadiga <frizadiga@gmail.com>
2017-04-13 18:47:34 +0700

    enhancement

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './w3.css';
+import hiuwave from './hiuwave.png';
 
 class App extends Component {
+
+ 
   render() {
+   const taglineStyle = {
+  marginTop: 64
+  }
     return (
-      <div className="App">
+      <div className="App w3-blue-grey">
         <div className="App-header w3-container w3-blue-grey">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={hiuwave} className="w3-animate-fading" alt="hiuWave" />
+          {/*<img src={logo} className="App-logo w3-text-black" alt="logo" />*/}
           <h2>hiuReact</h2>
+          <div className="w3-content">
+          <input id="text1" className="w3-input w3-center" type="text"/>
+          <button className="w3-button w3-block w3-hover-green w3-large">Try It!</button>
+          </div>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          
+        <p className="w3-medium" style={taglineStyle}>
+         just another react project
         </p>
       </div>
     );
