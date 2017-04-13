e8b28e624f215b0d387b8c2b5fa7e2fee2b9409c
frizadiga <frizadiga@gmail.com>
2017-04-13 16:51:09 +0700

    3rd update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './w3.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <div className="App-header w3-container w3-blue-grey">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to hiuReact</h2>
+          <h2>hiuReact</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
