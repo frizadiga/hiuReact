fb8c726be5b8ddd79069d548fdffefd397bcb1d3
frizadiga <frizadiga@gmail.com>
2017-04-13 19:13:43 +0700

    comment unused var

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import './w3.css';
 import hiuwave from './hiuwave.png';
